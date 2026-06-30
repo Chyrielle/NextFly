8fe94bbc219ba2be3ab66a9d3668b73cd5b10fa1
Calmantha <khususmantha@gmail.com>
2026-06-30 16:53:53 +0800

    Update admin.php

--- middleware/admin.php
+++ middleware/admin.php
@@ -1,1 +1,14 @@
+<?php
 
+require_once __DIR__ . '/auth.php';
+
+if (
+    !isset($_SESSION['role'])
+    ||
+    $_SESSION['role'] !== 'admin'
+) {
+
+    header("Location: beranda.php");
+    exit();
+
+}
