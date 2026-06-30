8bf1b1c3a4956d87b82cbd6e95d94c574ed263ee
Calmantha <khususmantha@gmail.com>
2026-06-30 18:01:09 +0800

    Update editor.php

--- middleware/editor.php
+++ middleware/editor.php
@@ -1,11 +1,15 @@
 <?php
 
 require_once __DIR__ . '/auth.php';
+require_once __DIR__ . '/../validation role/roles.php';
 
 if (
     !isset($_SESSION['role'])
     ||
-    !in_array($_SESSION['role'], ['admin', 'editor'])
+    !in_array(
+        $_SESSION['role'],
+        [ROLE_ADMIN, ROLE_EDITOR]
+    )
 ) {
 
     header("Location: beranda.php");
