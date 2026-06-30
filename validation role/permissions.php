e31cfa52c0692196c26767cf0392b3f5116daa51
Calmantha <khususmantha@gmail.com>
2026-06-30 17:17:30 +0800

    Create permissions.php

---
+++ validation role/permissions.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once __DIR__ . '/roles.php';
+
+$permissions = [
+
+    ROLE_ADMIN => [
+        'manage_users',
+        'manage_roles',
+        'manage_booking',
+        'manage_payment',
+        'view_reports'
+    ],
+
+    ROLE_EDITOR => [
+        'manage_booking',
+        'manage_payment'
+    ],
+
+    ROLE_VIEWER => [
+        'view_booking'
+    ]
+
+];
