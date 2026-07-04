096be8baefb0ad3efb75b876b5f3a8dd5f55a57e
Calmantha <khususmantha@gmail.com>
2026-07-04 14:25:16 +0800

    Update web_db.sql

--- config/web_db.sql
+++ config/web_db.sql
@@ -30,6 +30,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `username` varchar(100) NOT NULL,
+  `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `role` enum('admin','customer_service','user') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
