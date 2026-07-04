1c6a92f87ae5fc618f2ee62a0e11b6fba9853477
Calmantha <khususmantha@gmail.com>
2026-07-04 16:26:17 +0800

    Update CS.php

--- role/customer-service/CS.php
+++ role/customer-service/CS.php
@@ -12,19 +12,38 @@ if(time() > $_SESSION['expire']){
     header("location: ../../login.html");
     exit();
 }
+
 $role = $_SESSION['role'] ?? '';
-if (!isset($_SESSION['login']) || !in_array($role, ['admin', 'customer_service'])) {
+
+if (
+    !isset($_SESSION['login'])
+    ||
+    !in_array($role, ['admin', 'customer_service'])
+) {
     header("location: ../../login.html");
     exit();
 }
+
 echo "Dashboard Customer Service";
 ?>
+
 <?php if ($_SESSION['role'] === 'admin'): ?>
-<form action="../admin/Admin.php" method="GET"><button type="submit">Admin</button></form>
-<form action="../user/User.php" method="GET"><button type="submit">User</button></form>
+
+<form action="../admin/Admin.php" method="GET">
+    <button type="submit">Admin</button>
+</form>
+
+<form action="../user/User.php" method="GET">
+    <button type="submit">User</button>
+</form>
+
 <?php endif; ?>
 
-<?php if ($_SESSION['role'] === 'customer_service'): ?>
-<form action="../customer_service/Customer_Service.php" method="GET"><button type="submit">Customer_Service</button></form>
-<?php endif; ?>
+<form action="reports.php" method="GET">
+    <button type="submit">Kelola Report</button>
+</form>
+
+<form action="history.php" method="GET">
+    <button type="submit">Riwayat Report</button>
+</form>
 
