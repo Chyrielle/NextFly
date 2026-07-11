bba847e4cd99dd8709161e98072d1f95a7528c7c
Yogapratama889 <pratayoga56@gmail.com>
2026-07-11 20:06:08 +0800

    Update payment.php

--- role/user/payment.php
+++ role/user/payment.php
@@ -73,13 +73,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $user = mysqli_fetch_assoc($query);
         $email = $user['email'];
         
- echo "<pre>";
-        echo "user_id yang dipakai: [" . $user_id . "]\n";
-        echo "email yang ditemukan: [" . $email . "]\n";
-        echo "isi session: ";
-        print_r($_SESSION);
-        echo "</pre>";
-        die();
+
         
         if (sendTransactionEmail(
             $email,
